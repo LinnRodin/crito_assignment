8b04e1b2bdf99003fbd7e0af330e336adb606cab
Linn Rodin Sjögren <ael.rodin@gmail.com>
2023-09-03 21:58:04 +0200

    Article and News

---
+++ js/script.js
@@ -0,0 +1,22 @@
+/* $(document).ready(function(){
+    // Carousel for articles and news
+    $('.carousel-article').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        centerMode: true,
+        variableWidth: true
+    });
+    
+    // Carousel for team 
+    $('.carousel-slides').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        centerMode: true,
+        variableWidth: true
+    });
+});
+ */
